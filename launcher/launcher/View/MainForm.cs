664763b9adfb192b3bf32a32a14cb3f78bfe311f
34736384 <34736384@users.noreply.github.com>
2023-12-23 02:00:23 -0500

    add launcher project files

---
+++ launcher/launcher/View/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace launcher.View
+{
+    public partial class MainForm : Form
+    {
+        private bool _dragging = false;
+        private Point _dragCursorPoint;
+        private Point _dragFormPoint;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+        }
+        
+    }
+}
