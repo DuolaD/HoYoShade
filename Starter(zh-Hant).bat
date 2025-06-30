ae170ac24c81122827fdcfce34aa67e4b4e7fee8
DuolaD <D_mou@outlook.com>
2025-06-30 22:28:15 +0800

    Add 'LauncherResource','reshade-shaders' and 'Presets' folders into HoYoShade Starter self-check logic.

--- Starter(zh-Hant).bat
+++ Starter(zh-Hant).bat
@@ -12,7 +12,7 @@ if %errorLevel% neq 0 (
     exit /b
 )
 
-set files_to_check=("inject.exe" "ReShade64.dll" "InjectResource" "convert_encoding.bat" "LauncherResource\INIBuild.exe" "InjectResource\Fonts\msyhbd.ttc")
+set files_to_check=("inject.exe" "ReShade64.dll" "InjectResource" "LauncherResource" "reshade-shaders" "Presets" "convert_encoding.bat" "LauncherResource\INIBuild.exe" "InjectResource\Fonts\msyhbd.ttc")
 
 setlocal enabledelayedexpansion
 cd /d "%~dp0"
